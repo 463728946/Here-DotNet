301bbd46026e8733560121abf07f66a82cf00c3d
463728946@qq.com <463728946@qq.com>
2021-06-29 02:07:28 -0700

    Summary

--- src/HereDotNet.Core/Response/RouteCalculateRequestResponse.cs
+++ src/HereDotNet.Core/Response/RouteCalculateRequestResponse.cs
@@ -23,9 +23,16 @@ namespace HereDotNet.Core.Response
         public string Type { get; set; }
         public Departure Departure { get; set; }
         public Arrival Arrival { get; set; }
+        public Summary Summary { get; set; }
         public Transport Transport { get; set; }
     }
 
+    public class Summary
+    {
+        public double Duration { get; set; }
+        public double Length { get; set; }
+        public double BaseDuration { get; set; }
+    }
     public class Departure
     {
         public string Type { get; set; }
